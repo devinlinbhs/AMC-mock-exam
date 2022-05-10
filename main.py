4c2e18bdff1a81b1286ae28e66ff6026fa08d544
devinlinbhs <19550@burnside.school.nz>
2022-05-10 23:38:20 +1200

    HTML for filtering created (with all codes adjusted) & Extra 30 data added

--- main.py
+++ main.py
@@ -18,15 +18,12 @@ def get_db():
     return db
 
 
-
 # Close connection from database
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-
 
 
 # Create a homepage (tempoaray right now)
@@ -36,23 +33,38 @@ def home():
     return render_template("home.html", active='home')
 
 
+@app.route("/setting_exam")
+def setting_exam():
+    return render_template("setting_exam.html")
+
+
 # Question page
 # For the users to answer muti-choice questions(Will be used for Radom Quizes and Past Exam Practices at some point)
-@app.route("/question")
+@app.route("/question", methods = ["GET","POST"])
 def question():
-    
+    year = request.form["year"] # Problem is here
+    difficulty = request.form["difficulty"]
+    if difficulty == "senior":
+        foreign_id = "3"
+    elif difficulty == "intermediate":
+        foreign_id = "2"
+    else:
+        foreign_id = "1"
+    file_location = year+'_'+difficulty
+    print(file_location)
+
     cursor = get_db().cursor()
-    query = "SELECT picture_file FROM question"
+    query = "SELECT picture_file FROM question WHERE year = ? AND difficulty = ?"
     # Create a cursor to deliver query in the database
 
-    cursor.execute(query)
+    cursor.execute(query,(year,foreign_id))
     information = cursor.fetchall()
+    print(information)
     # Get all the picture files in a tuple
     # Need a better way to get the name of the picture files when there is much more data in the database
+    
 
-    # Bring the global variable to local and set it to the first question
-
-    return render_template("question.html", information=information, active='question')
+    return render_template("question.html", information=information, active='setting_exam', file_location = file_location)
     # Lead to question.html, pass the picture_files to the website with the current question, and the current active website
 
 
@@ -65,29 +77,28 @@ def upload_user_answer():
 
     cursor.execute(query)
     information = cursor.fetchall()
-    
+
     query = "UPDATE question SET user_choice = ? WHERE picture_file = ?"
     # Update the user's answer query
-    
-    for i in range (30):
+
+    for i in range(30):
         current_question = i+1
-        #create a counting variable
-        
+        # create a counting variable
+
         try:
-            answer = request.form["answer"+ str(current_question)]
+            answer = request.form["answer" + str(current_question)]
 # Now bring the value of the "answer1" or "answer2" etc from the question.html, the user's choice for the question to the python file
-            
+
         except:
-            answer = '' 
-            # If there is no answer then there will be error, thus do this route 
-            
+            answer = ''
+            # If there is no answer then there will be error, thus do this route
+
         cursor.execute(query, (answer, information[current_question-1][0]))
         # Update the data into the database
-        
+
     get_db().commit()
     # Execute then save it
-    
-    
+
     return redirect("check_answer")
     # Move on to next question in question.html, pass down the same file name, active website question.html
 
@@ -97,37 +108,36 @@ def check_answer():
     cursor = get_db().cursor()
     query = "SELECT answer, user_choice FROM question"
 
-
     cursor.execute(query)
     marking_scheme = cursor.fetchall()
     # To get the user's answers VS the model answers in the database
 
     score = 0
     # Create a new variable
-    
+
     for count in range(30):
         question_number = count + 1
         # The real question number is always 'count' + 1
-        
+
         if 1 <= question_number <= 10:
             if marking_scheme[count][0] == marking_scheme[count][1]:
                 score += 3
-        # Question 1 ~ 10 worth 3 points each        
-                
+        # Question 1 ~ 10 worth 3 points each
+
         elif 11 <= question_number <= 20:
             if marking_scheme[count][0] == marking_scheme[count][1]:
                 score += 4
-        # Question 11 ~ 20 worth 4 points each         
-                
+        # Question 11 ~ 20 worth 4 points each
+
         elif 21 <= question_number <= 25:
             if marking_scheme[count][0] == marking_scheme[count][1]:
                 score += 5
-        # Qquestion 21 ~ 25 worth 4 points each             
-                
+        # Qquestion 21 ~ 25 worth 4 points each
+
         else:
             if marking_scheme[count][0] == marking_scheme[count][1]:
                 score += question_number - 20
-        # Q26 : 6 points; Q27 : 7 points; Q28 : 8 points; Q29 : 9 points; Q30 : 10 points, 
+        # Q26 : 6 points; Q27 : 7 points; Q28 : 8 points; Q29 : 9 points; Q30 : 10 points,
 
     return render_template("score.html", score=score)
 
