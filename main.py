b2d2731c50b646f74ba2376552a9d38f60aa3fa4
devinlinbhs <19550@burnside.school.nz>
2022-06-27 17:46:32 +1200

    Pre-sprint 2 database correction

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def setting_exam():
 
 
 app.secret_key = "asdfghjkl"
-
+#setting up the secret key for session
 
 
 
@@ -49,30 +49,43 @@ app.secret_key = "asdfghjkl"
 # For the users to answer muti-choice questions(Will be used for Radom Quizes and Past Exam Practices at some point)
 @app.route("/question", methods = ["GET","POST"])
 def question():
-    
-    session['year'] = request.form['year']
+
+    year = request.form['year']
     difficulty = request.form["difficulty"]
     # Get the values from the drop down menu
     
+    past_paper_id = 0
     if difficulty == "senior":
-        session['foreign_id'] = "3"
+        past_paper_id += 10
     elif difficulty == "intermediate":
-        session['foreign_id'] = "2"
+        past_paper_id += 5
     else:
-        session['foreign_id'] = "1"
-    # The corresponding foreignkey value in the database
+        past_paper_id += 0
+        
+    if year == "2020":
+        past_paper_id += 5
+    elif year == "2019":
+        past_paper_id += 4
+    elif year == "2018":
+        past_paper_id += 3
+    elif year == "2017":
+        past_paper_id += 2
+    elif year == "2016":
+        past_paper_id += 1
+    # The corresponding past_paper_id value in the database
     
-    file_location = session['year']+'_'+difficulty
+    session["past_paper_id"] = str(past_paper_id)
+    
+    file_location = year +'_'+difficulty
     # Create a string which is exactly the file location
 
     cursor = get_db().cursor()
-    query = "SELECT picture_file FROM question WHERE year = ? AND difficulty = ?"
+    query = "SELECT picture_file FROM question WHERE past_paper_id = ?"
     # Create a cursor to deliver query in the database
 
-    cursor.execute(query,(session['year'],session['foreign_id']))
+    cursor.execute(query,(past_paper_id,))
     information = cursor.fetchall()
     # Get all the picture files in a tuple
-    
     
     return render_template("question.html", information=information, active='setting_exam', file_location = file_location)
     # Lead to question.html, pass the picture_files to the website with the current question, and the current active website
@@ -83,9 +96,9 @@ def question():
 def upload_user_answer():
     
     cursor = get_db().cursor()
-    query = "SELECT picture_file FROM question WHERE year = ? AND difficulty = ?"
+    query = "SELECT picture_file FROM question WHERE past_paper_id = ?"
     
-    cursor.execute(query,(session['year'], session['foreign_id']))
+    cursor.execute(query,(session['past_paper_id'],))
     information = cursor.fetchall()
     
     
@@ -109,10 +122,10 @@ def upload_user_answer():
     get_db().commit()
 
     cursor = get_db().cursor()
-    query = "SELECT answer, user_choice FROM question WHERE year = ? AND difficulty = ?"
+    query = "SELECT answer, user_choice FROM question WHERE past_paper_id = ?"
 
 
-    cursor.execute(query,(session['year'], session['foreign_id']))
+    cursor.execute(query,(session['past_paper_id'],))
     session['marking_scheme'] = cursor.fetchall()
     # To get the user's answers VS the model answers in the database
 
@@ -157,32 +170,44 @@ def setting_quiz():
 def quiz():
     
     set = request.form["set"]
+    # set will determine what type of quiz we are doing
+    
     difficulty = request.form["difficulty"]
+    # difficulty will determine how hard the overall paper is
+
 
     file_location_set = []
+    # This will be passed to the website, which helps the "url_for" to find the picture file position
     
     session['marking_scheme'] = []
-    session['year_set'] = []
-    session['question_set'] = []
+    # This ensures the marking_scheme of the mixed list is passed to the marking route for 
+    # quiz without messing up the correct answer for the random questions
+    
+    question_set = []
+    # Create a list to store the name of the random questions' images
+    
+    past_paper_id = 0
+    # To create a past_paper_id calculation method
     
     if difficulty == "senior":
-        session['foreign_id'] = "3"
+        past_paper_id += 10
     elif difficulty == "intermediate":
-        session['foreign_id'] = "2"
+        past_paper_id += 5
     else:
-        session['foreign_id'] = "1"
+        past_paper_id += 0
     
     
     cursor = get_db().cursor()
-    query = "SELECT picture_file, answer FROM question WHERE year = ? AND difficulty = ? AND question_number = ?"
+    query = "SELECT picture_file, answer FROM question WHERE past_paper_id = ? AND question_number = ?"
+    # This will be the query we use every time in the for loop
+    # Get the image name and also the correct answer from the database
     
-        
     
     if set == "set1":
         constant = 1
         session['run'] = 10
         # Constant will be add to the {i} to get the question number we supposed to be on
-        # session['year_set'] determine how many times we run the for loop
+        # "set" determine how many times we run the for loop
         
     elif set == "set2":
         constant = 11
@@ -195,18 +220,25 @@ def quiz():
     for i in range (session['run']):
         question_number = i + constant
         year = random.randint(2016, 2020)
-        session['year_set'].append(year)
         
-        cursor.execute(query,(year,session['foreign_id'], question_number))
+        past_paper_id_value = str(past_paper_id + year - 2015)
+        # This is the actual past_paper_id we put into the query to select the corresponding image
+        
+        cursor.execute(query,(past_paper_id_value, question_number))
         random_question = cursor.fetchall()
 
-        session['question_set'].append(random_question[0][0])
+        question_set.append(random_question[0][0])
+        # Store the name of the image into the list, so we can recall the 
+        # names/locations of the images in HTML
+        
         session['marking_scheme'].append(random_question[0][1])
+        # Store the correct answer and pass onto the marking route
         
         file_location = str(year)+'_'+difficulty
         file_location_set.append(file_location)
+        # Adding the file location of each image correspondingly to recall them in the HTML
 
-    return render_template("quiz.html", question_set = session['question_set'], file_location_set = file_location_set,
+    return render_template("quiz.html", question_set = question_set, file_location_set = file_location_set,
                             run = session['run'], constant = constant, active = "setting_quiz")
 
 
@@ -228,10 +260,12 @@ def upload_user_answer_quiz():
 
     score = 0
     # Create a new variable
+    
     if session['run'] == 5:
         type = "quiz_5"
     else:
         type = "quiz_10"
+    # This tells the HTML what is the mark actually out of
     
     for count in range(session['run']):
         if session['marking_scheme'][count] == answer_set[count]:
